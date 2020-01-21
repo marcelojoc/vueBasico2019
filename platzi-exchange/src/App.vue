c2424c15de983dce23134b09f4ed1836a7b8cdfe
marceloNotebook <marcelojoc@gmail.com>
2020-01-21 12:53:58 -0300

    creando  componente  header  icon y  table

--- platzi-exchange/src/App.vue
+++ platzi-exchange/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+
+  <PxHeader> </PxHeader>
+  <PxAssetsTable> </PxAssetsTable>
+
+
+
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
+import PxHeader from "@/components/PxHeader"
+import PxAssetsTable from "@/components/PxAssetsTable"
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  components: { PxHeader, PxAssetsTable
+ 
   }
 };
 </script>
